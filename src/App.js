5e2752df03eef91fb7e7834e8b391e6878b71443
Yejin <kyj5840kyj@duksung.ac.kr>
2023-11-04 16:39:08 +0900

    #7 Feat: 주요처리페이지 라우터 설정

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Menu from "./components/menu/Menu";
 import SearchPage from "./pages/SearchPage";
 import RecycleExplain from "./pages/RecycleExplainPage";
 import TrashCanList from "./pages/TrashCanListPage";
+import MainExample from "./pages/MainExample";
+import MainExampleDetail from "./pages/MainExampleDetail";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
             <Route path="/search" element={<SearchPage />} />
             <Route path="/explain" element={<RecycleExplain />} />
             <Route path="/trashcan" element={<TrashCanList />} />
+            <Route path="/mainexample" element={<MainExample />} />
+            <Route path="/mainexample/detail" element={<MainExampleDetail />} />
           </Routes>
         </div>
       </BrowserRouter>
