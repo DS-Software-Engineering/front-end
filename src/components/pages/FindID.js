e8269eec23e6ac7eb23b6e5f3f3220bd4f084415
minseochoo <cloudiao3o@gmail.com>
2023-11-07 15:35:55 +0900

    #11 Feat: 아이디/비밀번호 찾기 페이지 화면 구현

---
+++ src/components/pages/FindID.js
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+function FindID() {
+  return (
+    <Container>
+      <NavBox>
+        <NavDiv>
+          <NavSpan style={{ fontWeight: "700" }}>ID 찾기</NavSpan>
+        </NavDiv>
+        <NavDivider></NavDivider>
+        <NavDiv2>
+          <NavSpan>PW 찾기</NavSpan>
+        </NavDiv2>
+      </NavBox>
+      <InnerBox>
+        <SubTitle>휴대폰 번호*</SubTitle>
+        <InputBox>
+          <Input
+            type="text"
+            placeholder="가입할 때 입력한 번호를 입력해주세요."
+          />
+          <CheckBtn>인증 번호 발송</CheckBtn>
+        </InputBox>
+        <SubTitle>휴대폰 본인 인증*</SubTitle>
+        <InputBox>
+          <Input type="text" placeholder="인증번호 6자리를 입력해주세요." />
+          <CheckBtn>확인</CheckBtn>
+        </InputBox>
+      </InnerBox>
+      <FindIDBtn>ID 찾기</FindIDBtn>
+      <NoticeID />
+    </Container>
+  );
+}
+
+function NoticeID() {
+  return (
+    <NoticeIDBox>
+      <h4>회원님의 아이디는 ******* 입니다.</h4>
+      <GotoLoginBtn>로그인 하러 가기</GotoLoginBtn>
+    </NoticeIDBox>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 80px;
+`;
+
+const InnerBox = styled.div`
+  width: 350px;
+  height: 160px;
+`;
+
+const FindIDBtn = styled.button`
+  width: 350px;
+  height: 55px;
+  border-radius: 15px;
+  background-color: #dceeff;
+  border: none;
+  font-size: large;
+  font-weight: 700;
+  margin-top: 25px;
+`;
+
+const NavBox = styled.div`
+  width: 300px;
+  height: 55px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px 0px 20px 0px;
+`;
+
+const NavDiv = styled.div`
+  width: 100px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+`;
+
+const NavDiv2 = styled.div`
+  width: 100px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+`;
+
+const NavSpan = styled.span`
+  font-size: large;
+`;
+
+const NavDivider = styled.div`
+  width: 1px;
+  height: 35px;
+  background-color: black;
+  margin: 0 35px;
+`;
+
+const SubTitle = styled.span`
+  font-size: medium;
+  font-weight: 600;
+  color: #626262;
+  margin: 10px 0;
+`;
+
+const Input = styled.input`
+  width: 220px;
+  height: 30px;
+  border: none;
+  border-bottom: 1px solid gray;
+  font-size: small;
+  margin-bottom: 15px;
+`;
+
+const CheckBtn = styled.button`
+  width: 115px;
+  height: 30px;
+  border-radius: 10px;
+  border: none;
+  background-color: #dddddd;
+  color: white;
+  font-weight: 700;
+  font-size: medium;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px 0px;
+`;
+
+const NoticeIDBox = styled.div`
+  width: 350px;
+  margin-top: 25px;
+  border: 1px solid #383838;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const GotoLoginBtn = styled.button`
+  width: 310px;
+  height: 50px;
+  background-color: white;
+  border-radius: 30px;
+  border: 1px solid #1d70b6;
+  font-size: small;
+  text-decoration: underline;
+  margin-bottom: 20px;
+`;
+
+export default FindID;
---
+++ src/components/pages/Terms.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { AiFillCheckCircle } from "react-icons/ai";
+
+function Terms() {
+  return (
+    <Container>
+      <TitleSpan>약관 동의</TitleSpan>
+      <LineBox>
+        <InnerBox>
+          <AiFillCheckCircle className="check-icon" color="#1d70b6" />
+          <BigAgreeText>약관 전체 동의</BigAgreeText>
+        </InnerBox>
+        <TermBox choice="필수" />
+        <TermBox choice="필수" />
+        <TermBox choice="선택" />
+      </LineBox>
+      <RegisterBtn>가입하기</RegisterBtn>
+    </Container>
+  );
+}
+
+function TermBox({ choice }) {
+  const [check, setCheck] = useState(false);
+
+  const handleCheck = () => {
+    setCheck(!check);
+  };
+  return (
+    <>
+      <InnerBox onClick={handleCheck}>
+        {check ? (
+          <AiFillCheckCircle
+            className="check-icon"
+            style={{ color: "#1d70b6" }}
+          />
+        ) : (
+          <AiFillCheckCircle
+            className="check-icon"
+            style={{ color: "#dddddd" }}
+          />
+        )}
+        <SmallAgreeText>[{choice}] 이용 약관에 동의합니다.</SmallAgreeText>
+      </InnerBox>
+      <TermContent>
+        TRAP 서비스 제공을 위해 회원번호와 함께 개인정보가 제공됩니다. 보다
+        자세한 제인정보 제공항목은 동의 내용에서 확인하실 수 있습니다. 해당
+        정보는 동의 철회 또는 서비스 탈퇴 시 지체없이 파기됩니다. TRAP 서비스
+        제공을 위해 회원번호와 함께 개인정보가 제공됩니다. 보다 자세한 제인정보
+        제공항목은 동의 내용에
+      </TermContent>
+    </>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 80px;
+`;
+
+const TitleSpan = styled.div`
+  font-size: larger;
+  font-weight: 600;
+  margin: 30px 0px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const LineBox = styled.div`
+  width: 320px;
+  height: 430px;
+  border: 1px solid #b8b8b8;
+  border-radius: 15px;
+  padding: 5px 15px 20px 15px;
+`;
+
+const BigAgreeText = styled.span`
+  color: #8a8a8a;
+  font-size: larger;
+  font-weight: 700;
+`;
+
+const SmallAgreeText = styled.div`
+  color: #8a8a8a;
+  font-size: medium;
+`;
+
+const InnerBox = styled.div`
+  .check-icon {
+    margin-right: 10px;
+    width: 25px;
+    height: 25px;
+    color: #dddddd;
+  }
+  display: flex;
+  align-items: center;
+  margin: 15px 0px;
+`;
+
+const TermContent = styled.textarea`
+  border: 1px solid #bdd7c9;
+  border-radius: 10px;
+  font-size: small;
+  width: 303px;
+  height: 55px;
+  padding: 8px;
+`;
+
+const RegisterBtn = styled.button`
+  width: 350px;
+  height: 55px;
+  border-radius: 15px;
+  background-color: #dceeff;
+  border: none;
+  font-size: large;
+  font-weight: 700;
+  margin-top: 25px;
+`;
+
+export default Terms;
