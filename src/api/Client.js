ed3382a29de4955ecf59df307bce3484210a466c
Yejin <kyj5840kyj@duksung.ac.kr>
2023-11-13 22:11:35 +0900

    #24 Feat: API 세팅

---
+++ src/api/Client.js
@@ -0,0 +1,45 @@
+const baseURL = "http://localhost:8080";
+
+const baseHeaders = {
+  "Content-Type": "application/json",
+};
+
+const getHeadersWithToken = () => {
+  const token = localStorage.getItem("token");
+  return token
+    ? { ...baseHeaders, Authorization: `Bearer ${token}` }
+    : { ...baseHeaders };
+};
+
+const sendAuthorizedRequest = async ({ url, headers, method, data }) => {
+  const requestOptions = {
+    method,
+    headers: { ...getHeadersWithToken(), ...headers },
+  };
+
+  if (data) {
+    requestOptions.body = JSON.stringify(data);
+  }
+
+  const response = await fetch(baseURL + url, requestOptions);
+  return response.json();
+};
+
+const createRequestMethod = (method) => async (requestConfig) => {
+  try {
+    const response = await sendAuthorizedRequest({ ...requestConfig, method });
+    // Handle the response as needed
+    console.log(response);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+  }
+};
+
+export const request = {
+  get: createRequestMethod("get"),
+  post: createRequestMethod("post"),
+  put: createRequestMethod("put"),
+  patch: createRequestMethod("patch"),
+  delete: createRequestMethod("delete"),
+};
