2accb017e14c4353a14080660b2d01457f1f0f8f
minseochoo <cloudiao3o@gmail.com>
2023-11-15 04:33:43 +0900

    #13 Feat: 마이페이지 화면 구현

--- src/components/pages/ReportWaterwayHistory.js
+++ src/components/pages/ReportWaterwayHistory.js
@@ -1,8 +1,77 @@
 import React from "react";
 import styled from "styled-components";
+import ListComponent from "../main/ListComponent";
 
 function ReportWaterwayHistory() {
-  return <>배수구</>;
+  return (
+    <Container>
+      <TitleSpan>신고 내역</TitleSpan>
+      <TapBox>
+        <TapBtn
+          onClick={() => {
+            window.location.href = "/mypage/report/trash";
+          }}
+        >
+          무단투기
+        </TapBtn>
+        <SelectedTapBtn>배수구</SelectedTapBtn>
+      </TapBox>
+      <ListBox>
+        <ListComponent title="장소명1" category="음료 컵" address="상세주소" />
+        <ListComponent title="장소명2" category="음료 컵" address="상세주소" />
+        <ListComponent title="장소명3" category="음료 컵" address="상세주소" />
+        <ListComponent title="장소명4" category="음료 컵" address="상세주소" />
+        <ListComponent title="장소명5" category="음료 컵" address="상세주소" />
+      </ListBox>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TitleSpan = styled.div`
+  font-size: larger;
+  font-weight: 600;
+  margin: 30px 0px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const TapBox = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  margin-bottom: 18px;
+`;
+
+const SelectedTapBtn = styled.button`
+  width: 100px;
+  height: 30px;
+  background-color: #dceeff;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0px 5px 5px lightgray;
+  margin: 0px 7px;
+`;
+
+const TapBtn = styled.button`
+  width: 100px;
+  height: 30px;
+  background-color: white;
+  border: 0.1px solid lightgray;
+  border-radius: 15px;
+  box-shadow: 0px 3px 3px lightgray;
+  margin: 0px 7px;
+`;
+
+const ListBox = styled.div`
+  width: 100%;
+  position: relative;
+`;
 
 export default ReportWaterwayHistory;
--- src/components/pages/Reward.js
+++ src/components/pages/Reward.js
@@ -1,8 +1,73 @@
 import React from "react";
 import styled from "styled-components";
+import ListComponent from "../main/ListComponent";
 
 function Reward() {
-  return <>리워드</>;
+  return (
+    <Container>
+      <TitleSpan>리워드</TitleSpan>
+      <RewardBox>
+        <RewardIcon>R</RewardIcon>
+        <RewardSpan>200</RewardSpan>
+      </RewardBox>
+      <TitleSpan>리워드 적립 내역</TitleSpan>
+
+      <ListBox>
+        <ListComponent title="장소명1" category="음료 컵" address="상세주소" />
+        <ListComponent title="장소명2" category="음료 컵" address="상세주소" />
+        <ListComponent title="장소명3" category="음료 컵" address="상세주소" />
+      </ListBox>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+const TitleSpan = styled.div`
+  font-size: larger;
+  font-weight: 600;
+  margin: 30px 0px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const RewardBox = styled.div`
+  width: 210px;
+  height: 75px;
+  border: 1px solid #ffdf6c;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 35px;
+`;
+
+const RewardIcon = styled.div`
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+  background-color: #ffdf6c;
+  color: white;
+  font-size: 28px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 15px;
+`;
+
+const RewardSpan = styled.span`
+  font-size: x-large;
+`;
+
+const ListBox = styled.div`
+  width: 100%;
+  position: relative;
+`;
 
 export default Reward;
