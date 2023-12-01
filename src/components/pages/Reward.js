a08e1dc3fe4fdfbe63e3c5e840dfde4117374d1e
minseochoo <cloudiao3o@gmail.com>
2023-12-01 22:58:16 +0900

    #49 Feat: 리워드 내역 API 연동

--- src/components/pages/Reward.js
+++ src/components/pages/Reward.js
@@ -1,43 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import RewardListComponent from "../reward/RewardListComponent";
+import RewardListComponent from "../mypage/RewardListComponent";
 import { getReward } from "../../api/Mypage";
 
 function Reward() {
+  const [rewardAmount, setRewardAmount] = useState(0);
+  const [rewardHistory, setRewardHistory] = useState([]);
+
+  useEffect(() => {
+    fetchRewardData();
+  }, []);
+
+  const fetchRewardData = async () => {
+    try {
+      const response = await getReward();
+      if (response.status === 200) {
+        const data = response.data[0];
+        setRewardAmount(data.reduce((total, item) => total + item.reward, 0));
+        setRewardHistory(data);
+      } else {
+        console.error("리워드 정보 요청 실패:", response.message);
+      }
+    } catch (error) {
+      console.error("리워드 정보 요청 오류:", error);
+    }
+  };
+
   return (
     <Container>
       <TitleSpan>리워드</TitleSpan>
       <RewardBox>
         <RewardIcon>R</RewardIcon>
-        <RewardSpan>200</RewardSpan>
+        <RewardSpan>{rewardAmount}</RewardSpan>
       </RewardBox>
       <TitleSpan>리워드 적립 내역</TitleSpan>
 
       <ListBox>
-        <RewardListComponent
-          title="장소명1"
-          date="2023.09.25"
-          reward="+50 적립"
-          address="상세주소"
-        />
-        <RewardListComponent
-          title="장소명2"
-          date="2023.09.25"
-          reward="+50 적립"
-          address="상세주소"
-        />
-        <RewardListComponent
-          title="장소명3"
-          date="2023.09.25"
-          reward="+50 적립"
-          address="상세주소"
-        />
-        <RewardListComponent
-          title="장소명3"
-          date="2023.09.25"
-          reward="+50 적립"
-          address="상세주소"
-        />
+        {rewardHistory.map((item) => (
+          <RewardListComponent
+            title={item.address}
+            date={item.date}
+            reward={`+${item.reward} 적립`}
+            address={item.detail_location}
+          />
+        ))}
       </ListBox>
     </Container>
   );
